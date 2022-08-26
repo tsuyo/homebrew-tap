796b515eab54eafea2cfa3600ef5b23483e5db67
Tsuyoshi Miyake <dev.tsuyo@gmail.com>
2022-08-26 22:30:54 +0900

    fix cloudera-boot formula

--- cloudera-boot.rb
+++ cloudera-boot.rb
@@ -2,10 +2,9 @@ class ClouderaBoot < Formula
   desc "cloudera boot"
   homepage "https://github.com/tsuyo/cloudera-boot"
   url "https://github.com/tsuyo/homebrew-tap/raw/master/archive/cloudera-boot-6.0.0.0.tar.gz"
-  sha256 "79fee67087d5aab67f791bb9b5d28703559ad5f3160493cfb9a979460f66eaed"
+  sha256 "90b23fede3faba19b11b83ed525f2aaa57878f295c65ff5389ec2fd24190cc8a"
   version "6.0.0.0"
-
-  bottle :unneeded
+  license "MIT"
 
   def install
     bin.install "cboot"
