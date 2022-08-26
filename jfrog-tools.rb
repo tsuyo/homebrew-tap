a87989a57b20936ccda49e17ae2ee610efb95b96
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2022-08-26 23:05:35 +0900

    fix jf-delete-proj script

--- jfrog-tools.rb
+++ jfrog-tools.rb
@@ -1,9 +1,9 @@
 class JfrogTools < Formula
   desc "jfrog tools"
   homepage "https://github.com/tsuyo/jfrog-tools"
-  url "https://github.com/tsuyo/homebrew-tap/raw/master/archive/jfrog-tools-0.0.1.tar.gz"
-  sha256 "c911260550a4e88786e2509eaf25fc11015484627fe3baeb5bc096016659aa54"
-  version "0.0.1"
+  url "https://github.com/tsuyo/homebrew-tap/raw/master/archive/jfrog-tools-0.0.2.tar.gz"
+  sha256 "20eb4c7cb24329c4fffd240a653875b6bc619a844b95424a94c9f7cfe3910bc0"
+  version "0.0.2"
   license "MIT"
 
   def install
