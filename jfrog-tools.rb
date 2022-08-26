9abcea8b11ee5cba3f6524256f5272cbab88f29b
Tsuyoshi Miyake <dev.tsuyo@gmail.com>
2022-08-26 22:19:16 +0900

    add license to jfrog-tools formula 2

--- jfrog-tools.rb
+++ jfrog-tools.rb
@@ -2,9 +2,9 @@ class JFrogTools < Formula
   desc "jfrog tools"
   homepage "https://github.com/tsuyo/jfrog-tools"
   url "https://github.com/tsuyo/homebrew-tap/raw/master/archive/jfrog-tools-0.0.1.tar.gz"
-  sha256 "8570d202641491a22fc2a6a1c36ee2961b006a0c0e9b4aa7805b411d3925cf8c"
+  sha256 "0a3385c676d1b3518cce5c2aacd8ef1f383cf8624ff9559b649fd487565ce6d8"
   version "0.0.1"
-  license ""
+  license "MIT"
 
   bottle :unneeded
 
