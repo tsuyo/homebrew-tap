d85878bafcb21e2fdab7fa6c38cd4c252febeb45
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2022-08-31 14:42:00 +0900

    add template-cloudera-boot.rb

--- template-cloudera-boot.rb
+++ template-cloudera-boot.rb
@@ -1,8 +1,8 @@
 class ClouderaBoot < Formula
   desc "cloudera boot"
   homepage "https://github.com/tsuyo/cloudera-boot"
-  url "https://github.com/tsuyo/cloudera-boot/archive/refs/tags/v6.0.0.0.tar.gz"
-  sha256 "8155420ca11a9bbd12e98b9d73995e1a5fbc7064a658e253cf5ff95cf30b9e7f"
+  url "${URL}"
+  sha256 "${SHA256}"
   license "MIT"
 
   def install
