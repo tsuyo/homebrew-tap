4848238b8cd58013b6bf8ad9008543079c31df73
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2022-09-01 16:49:10 +0900

    release jfrog-tools v0.0.4

--- jfrog-tools.rb
+++ jfrog-tools.rb
@@ -1,8 +1,8 @@
 class JfrogTools < Formula
   desc "jfrog tools"
   homepage "https://github.com/tsuyo/jfrog-tools"
-  url "https://github.com/tsuyo/jfrog-tools/archive/refs/tags/v0.0.3.tar.gz"
-  sha256 "570ebe7f4643a2322c58497381c68e716db11020d0c7792170544a49f6bfe259"
+  url "https://github.com/tsuyo/jfrog-tools/archive/refs/tags/v0.0.4.tar.gz"
+  sha256 "b1d8dfa4a5f0e4e04d37f4b6c92166087aea24b201717686b38de4a1ace68249"
   license "MIT"
 
   depends_on "jq"
@@ -14,5 +14,16 @@ class JfrogTools < Formula
     bin.install "jf-quick-setup"
     bin.install "jf-quick-teardown"
     bin.install "jf-get-versions"
+    bin.install "jf-docker-build"
+    bin.install "jf-go-clean"
+    bin.install "jf-go-build"
+    bin.install "jf-gradle-clean"
+    bin.install "jf-gradle-build"
+    bin.install "jf-mvn-clean"
+    bin.install "jf-mvn-build"
+    bin.install "jf-npm-clean"
+    bin.install "jf-npm-build"
+    bin.install "jf-pip-clean"
+    bin.install "jf-pip-build"
   end
 end
--- template-jfrog-tools.rb
+++ template-jfrog-tools.rb
@@ -14,5 +14,16 @@ class JfrogTools < Formula
     bin.install "jf-quick-setup"
     bin.install "jf-quick-teardown"
     bin.install "jf-get-versions"
+    bin.install "jf-docker-build"
+    bin.install "jf-go-clean"
+    bin.install "jf-go-build"
+    bin.install "jf-gradle-clean"
+    bin.install "jf-gradle-build"
+    bin.install "jf-mvn-clean"
+    bin.install "jf-mvn-build"
+    bin.install "jf-npm-clean"
+    bin.install "jf-npm-build"
+    bin.install "jf-pip-clean"
+    bin.install "jf-pip-build"
   end
 end
